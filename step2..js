3377d10ebedcf3c6c26b5addc4c666a13e235ee0
gannonh <gannonh@users.noreply.github.com>
2023-01-12 12:54:32 -0800

    oops

--- step2..js
+++ step2..js
@@ -1,15 +1,24 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-client.on("messageCreate", function(message) {
-   if (message.author.bot) return;
-   return message.reply(`${message.content}`);                          
-
-});                               
+client.on("messageCreate", function (message) {
+  if (message.author.bot) return;
+  return message.reply(`${message.content}`);
+});
 client.login(process.env.BOT_TOKEN);
